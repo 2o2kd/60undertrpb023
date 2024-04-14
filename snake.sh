1a6e251f807bb21ed0f1d4e5ccd35b5d215d5375
2o2kd <166863822+2o2kd@users.noreply.github.com>
2024-04-14 19:30:11 -0300

    Update snake.sh

--- snake.sh
+++ snake.sh
@@ -24,3 +24,15 @@ fi
 # Ativa o wake lock e inicia o servidor SSH
 termux-wake-lock
 sshd
+
+# Acessa a distribuição Debian e executa os comandos adicionais
+proot-distro login debian <<EOF
+apt update -y
+apt upgrade -y
+apt install sudo -y
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+sudo apt update
+sudo apt install ngrok -y
+ngrok config add-authtoken 2evcdAeONYR2HK56AQC4bGNk61Z_6fQNMhSpeUkNaj2mU4rcV
+EOF
